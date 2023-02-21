55abc65143a7b90a904463a509ce128daa0119d1
OzzkanBulut <ozkanb66@gmail.com>
2023-02-21 17:37:54 +0300

    Initial Commit

---
+++ _1_PassByValuewithPrimitives/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Pass by value
+        int age = 21;
+        int ageCopy = age;
+        System.out.println(age);
+        System.out.println(ageCopy);
+    }
+}
