12b68f5c580543765c985d96d673a4d49e72fd52
OzzkanBulut <ozkanb66@gmail.com>
2023-02-21 17:43:20 +0300

    Pass by Value with Primitive Types

--- _1_PassByValuewithPrimitives/src/Main.java
+++ _1_PassByValuewithPrimitives/src/Main.java
@@ -2,8 +2,18 @@ public class Main {
     public static void main(String[] args) {
         // Pass by value
         int age = 21;
-        int ageCopy = age;
+        int ageCopy = age; // value of age is copied to ageCopy
         System.out.println(age);
         System.out.println(ageCopy);
+        ageCopy = 22;
+        System.out.println(ageCopy);
+        System.out.println(age); // age is not affected by the change of ageCopy
+        ageCopy = increment(age);
+        System.out.println(ageCopy); // 22
+        System.out.println(age); // Again , age will not change
+
+    }
+    static int increment(int age){
+        return ++age;
     }
 }
