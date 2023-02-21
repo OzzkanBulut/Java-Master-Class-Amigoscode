2aac7e0f4bd1ca5694d0b5fa614c32b935cb18a2
OzzkanBulut <ozkanb66@gmail.com>
2023-02-21 22:24:09 +0300

    If

---
+++ _6_IfStatements/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 18;
+        boolean isAdult = age >=18;
+        if(isAdult){
+            System.out.println("You are an adult!");
+        }else if(age==1){
+            System.out.println("You are a baby!");
+        }else if(age==2){
+            System.out.println("You are a toddler!");
+        }else if(age==3){
+            System.out.println("You are a toddler!");
+        }else {
+            System.out.println("You are not human!");
+
+        }
+    }
+}
