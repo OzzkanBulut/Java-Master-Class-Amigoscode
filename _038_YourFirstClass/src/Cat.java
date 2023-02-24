a9f76408d50193b4ea5aef176c0117b86cff3166
OzzkanBulut <ozkanb66@gmail.com>
2023-02-24 18:09:09 +0300

    Classes

---
+++ _038_YourFirstClass/src/Cat.java
@@ -0,0 +1,15 @@
+public class Cat{ // cat class must be static in order to instantiate in a static method
+    private String name;
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public void meow(){
+        System.out.println(name + ": meow...");
+    }
+
+}
---
+++ _038_YourFirstClass/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        Cat kitty = new Cat();
+        kitty.setName("kitty");
+        System.out.println(kitty.getName());
+
+//        kitty.name --> not reachable because name is private
+
+
+        Cat star = new Cat();
+        star.setName("star");
+        System.out.println( star.getName());
+
+    }
+
+
+}
+
