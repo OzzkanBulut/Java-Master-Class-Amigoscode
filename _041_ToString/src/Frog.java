86a657d4fa8aae441b3aa071a2de17d1c1643f9a
OzzkanBulut <ozkanb66@gmail.com>
2023-02-24 19:26:29 +0300

    toString

---
+++ _041_ToString/src/Frog.java
@@ -0,0 +1,33 @@
+public class Frog {
+    private String name;
+    private String color;
+
+    public Frog() {
+    }
+
+    public Frog(String name, String color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: "+name+ " Color: " +color ;
+    }
+}
