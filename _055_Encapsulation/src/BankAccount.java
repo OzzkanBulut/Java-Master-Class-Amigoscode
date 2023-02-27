0d05332d929fbc362da005cdce93387acd3190e2
OzzkanBulut <ozkanb66@gmail.com>
2023-02-27 18:07:44 +0300

    Encapsulationn

--- _055_Encapsulation/src/BankAccount.java
+++ _055_Encapsulation/src/BankAccount.java
@@ -1,9 +1,25 @@
 import java.math.BigDecimal;
 import static java.math.BigDecimal.ZERO;
 public class BankAccount {
-    private String name;
-    private BigDecimal balance;
-    private boolean hasOverdraft;
+     String name;
+     BigDecimal balance;
+     boolean hasOverdraft;
+
+
+    public BankAccount() {
+    }
+
+    public BankAccount(String name) {
+        this.name = name;
+        this.hasOverdraft = false;
+        this.balance = ZERO;
+    }
+
+    public BankAccount(String name, BigDecimal balance, boolean hasOverdraft) {
+        this.name = name;
+        this.balance = balance;
+        this.hasOverdraft = hasOverdraft;
+    }
 
     // this is encapsulation.We are uniting data and behaviour in one place
     public BigDecimal withdrawMoney(BigDecimal amount){
--- _055_Encapsulation/src/Main.java
+++ _055_Encapsulation/src/Main.java
@@ -1,5 +1,23 @@
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount("özkan",
+                BigDecimal.TEN,
+                false);
+        System.out.println(
+                bankAccount.withdrawMoney(new BigDecimal("9.0")));
+        System.out.println(bankAccount.balance);
+        System.out.println();
+        System.out.println(
+                bankAccount.withdrawMoney(new BigDecimal("1.0")));
+        System.out.println(bankAccount.balance);
+
+        bankAccount.balance = new BigDecimal(100_000); // This is not good! There is no control here
+        System.out.println(bankAccount.balance); // anybody can reach the ballance
+
+
+
 
     }
 }
