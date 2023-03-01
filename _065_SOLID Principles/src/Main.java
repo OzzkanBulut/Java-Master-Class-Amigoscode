e895552543cbfc333f8f4fc20522b70d93bb59cc
OzzkanBulut <ozkanb66@gmail.com>
2023-03-01 19:58:23 +0300

    liskov prinmciple

--- _065_SOLID Principles/src/Main.java
+++ _065_SOLID Principles/src/Main.java
@@ -6,11 +6,18 @@ public class Main {
         Circle circle = new Circle(2);
         Square square = new Square(3);
         ShapesPrinter shapesPrinter = new ShapesPrinter();
+        Shape noShape = new NoShape(); // We violated the liskov principle.
+        // Subclasses must be substitutable to the superclass!
+        // noShape must not implement Shape interface!
 
-        List<Shape> shapes = List.of(circle,square);
+
+
+        List<Shape> shapes = List.of(circle,square,noShape);
         double sum = areaCalculator.sum(shapes);
         System.out.println(shapesPrinter.json(sum));
         System.out.println(shapesPrinter.csv(sum));
+
+
 
     }
 }
