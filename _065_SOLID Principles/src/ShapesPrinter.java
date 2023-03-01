c45d28cd0a577d651521edda55db4ca63a378fcf
OzzkanBulut <ozkanb66@gmail.com>
2023-03-01 21:58:52 +0300

    dependency inversion

--- _065_SOLID Principles/src/ShapesPrinter.java
+++ _065_SOLID Principles/src/ShapesPrinter.java
@@ -1,11 +1,21 @@
 import java.util.List;
 
 public class ShapesPrinter {
-    public String json(double sum){
-        return "{shapesSum : %s}".formatted(sum);
+    private AreaCalculator areaCalculator = new AreaCalculator();
+    // this is wrong. If you make a change in areacalculator then we actually are breaking the open close principle.
+    // ShapesPrinter class is depend on a concrete class.This is wrong!
+    //Instead, make AreaCalculator class an interface.
+    private final IAreaCalculator iAreaCalculator;
+
+    public ShapesPrinter(IAreaCalculator iAreaCalculator) {
+        this.iAreaCalculator = iAreaCalculator;
+    }
+
+    public String json(List<Shape> shapes){
+        return "{shapesSum : %s}".formatted(areaCalculator.sum(shapes));
   }
 
-    public String csv(double sum){
-        return "shapes_Sum,%s}".formatted(sum);
+    public String csv(List<Shape> shapes){
+        return "shapes_Sum,%s}".formatted(areaCalculator.sum(shapes));
     }
 }
