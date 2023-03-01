c87d3b9d42bb59ae74a63720260953d6639b216b
OzzkanBulut <ozkanb66@gmail.com>
2023-03-01 19:42:40 +0300

    main update

--- _065_SOLID Principles/src/Main.java
+++ _065_SOLID Principles/src/Main.java
@@ -1,5 +1,16 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        AreaCalculator areaCalculator = new AreaCalculator();
+        Circle circle = new Circle(2);
+        Square square = new Square(3);
+        ShapesPrinter shapesPrinter = new ShapesPrinter();
+
+        List<Object> shapes = List.of(circle,square);
+        double sum = areaCalculator.sum(shapes);
+        System.out.println(shapesPrinter.json(sum));
+        System.out.println(shapesPrinter.csv(sum));
+
     }
 }
--- _065_SOLID Principles/src/ShapesPrinter.java
+++ _065_SOLID Principles/src/ShapesPrinter.java
@@ -1,11 +1,11 @@
 import java.util.List;
 
 public class ShapesPrinter {
-    public String json(int sum){
+    public String json(double sum){
         return "{shapesSum : %s}".formatted(sum);
   }
 
-    public String csv(int sum){
+    public String csv(double sum){
         return "shapes_Sum,%s}".formatted(sum);
     }
 }
