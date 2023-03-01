b0879af67f53eca5716eb7217170b830f3887829
OzzkanBulut <ozkanb66@gmail.com>
2023-03-01 22:06:35 +0300

    Solid dependency inverison

--- _065_SOLID Principles/src/Main.java
+++ _065_SOLID Principles/src/Main.java
@@ -2,20 +2,23 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        AreaCalculator areaCalculator = new AreaCalculator();
+        IAreaCalculator areaCalculator = new AreaCalculator();
         Circle circle = new Circle(2);
         Square square = new Square(3);
-        ShapesPrinter shapesPrinter = new ShapesPrinter();
+        ShapesPrinter shapesPrinter = new ShapesPrinter(areaCalculator);
         Shape noShape = new NoShape(); // We violated the liskov principle.
         // Subclasses must be substitutable to the superclass!
         // noShape must not implement Shape interface!
 
 
 
-        List<Shape> shapes = List.of(circle,square,noShape);
+        List<Shape> shapes = List.of(circle,square);
         double sum = areaCalculator.sum(shapes);
-        System.out.println(shapesPrinter.json(sum));
-        System.out.println(shapesPrinter.csv(sum));
+        System.out.println(shapesPrinter.json(shapes));
+        System.out.println(shapesPrinter.csv(shapes));
+
+        ThreeDShape cube = new Cube(2);
+        System.out.println(cube.volume());;
 
 
 
