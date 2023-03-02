fe4017a9979172369bec146f2e11964c08ab5508
OzzkanBulut <ozkanb66@gmail.com>
2023-03-02 20:35:11 +0300

    GenericClasses

---
+++ _081_GenericAndClasses/src/Box.java
@@ -0,0 +1,10 @@
+public class Box <T>{ // T can be anything.Here, stands for Type.A convention
+    private T t;
+
+    public void setT(T t){
+        this.t = t;
+    }
+    public T getT(){
+        return t;
+    }
+}
---
+++ _081_GenericAndClasses/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+    Box<Integer> box = new Box<>();
+    box.setT(2);
+//    box.setT("as"); --> Not work! Box class only accepts Integer
+
+    }
+}
