186171d3572dbf278c34b999ee2b8b379a500d6b
OzzkanBulut <ozkanb66@gmail.com>
2023-03-02 20:27:59 +0300

    Type Parameter and Type Argument

---
+++ _080_TypeParameterandTypeArgument/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        // List<E> -->E is the Type Parameter
+        List<String> names = new ArrayList<>(); // String is the Type ARgument
+//        names.add(1) --> Does not work. names only accepts String
+        Map<String, String> map = new HashMap<>();
+        Map<Integer, String> map2 = new HashMap<>();
+
+
+
+    }
+}
