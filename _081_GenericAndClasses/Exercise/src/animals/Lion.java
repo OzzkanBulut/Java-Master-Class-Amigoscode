7c83535941fc70cb29277cb329ebe8a1d60bc525
OzzkanBulut <ozkanb66@gmail.com>
2023-03-04 23:30:04 +0300

    lion

---
+++ _081_GenericAndClasses/Exercise/src/animals/Lion.java
@@ -0,0 +1,18 @@
+package animals;
+
+public class Lion extends Animal{
+    private String lionSomething;
+
+    public Lion(String name, int age, boolean isWild) {
+        super(name, age, isWild);
+    }
+
+    public String getLionSomething() {
+        return lionSomething;
+    }
+
+    public void setLionSomething(String lionSomething) {
+        this.lionSomething = lionSomething;
+    }
+
+}
