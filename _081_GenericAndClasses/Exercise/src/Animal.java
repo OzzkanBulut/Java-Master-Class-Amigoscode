70346671e32f4f2ae059840e5af7e33a8a4cf93a
OzzkanBulut <ozkanb66@gmail.com>
2023-03-04 23:28:29 +0300

    animal class for exercise

---
+++ _081_GenericAndClasses/Exercise/src/Animal.java
@@ -0,0 +1,60 @@
+import java.util.Objects;
+
+public class Animal {
+    private String name;
+    private int age;
+    private boolean isWild;
+
+    public Animal(String name, int age, boolean isWild) {
+        this.name = name;
+        this.age = age;
+        this.isWild = isWild;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isWild() {
+        return isWild;
+    }
+
+    public void setWild(boolean wild) {
+        isWild = wild;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", isWild=" + isWild +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Animal animal = (Animal) o;
+        return age == animal.age && isWild == animal.isWild && Objects.equals(name, animal.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, isWild);
+    }
+
+}
