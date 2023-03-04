e7cefb45a7ce5e3f5df76985490900a192d573b4
OzzkanBulut <ozkanb66@gmail.com>
2023-03-04 23:32:14 +0300

    elelppelhant

---
+++ _081_GenericAndClasses/Exercise/src/animals/Elephant.java
@@ -0,0 +1,18 @@
+package animals;
+
+public class Elephant extends Animal{
+    private String elephantSomething;
+
+    public Elephant(String name, int age, boolean isWild) {
+        super(name, age, isWild);
+    }
+
+    public String getElephantSomething() {
+        return elephantSomething;
+    }
+
+    public void setElephantSomething(String elephantSomething) {
+        this.elephantSomething = elephantSomething;
+    }
+}
+
