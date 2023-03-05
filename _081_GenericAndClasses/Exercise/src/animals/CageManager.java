e7a554bfc785ddba8b7901ea3c4c25161535e02c
OzzkanBulut <ozkanb66@gmail.com>
2023-03-05 21:21:57 +0300

    cageManager turned into class

--- _081_GenericAndClasses/Exercise/src/animals/CageManager.java
+++ _081_GenericAndClasses/Exercise/src/animals/CageManager.java
@@ -2,8 +2,20 @@ package animals;
 
 import java.util.List;
 
-public interface CageManager {
-    List<Animal> getAnimals();
-    Animal getAnimal();
-    void setCage(List<Animal> animals);
+public class CageManager {
+
+    private List<Animal> animals;
+
+    public CageManager(List<Animal> animals) {
+        this.animals = animals;
+    }
+
+    List<Animal> getAnimals(){
+        return this.animals;
+    }
+
+    void setCage(List<Animal> animals){
+
+
+    }
 }
