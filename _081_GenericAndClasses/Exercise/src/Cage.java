bef0514f0d8ff3fa39070bf1ccee06310a4bec57
OzzkanBulut <ozkanb66@gmail.com>
2023-03-05 21:12:10 +0300

    Cage class for animals

---
+++ _081_GenericAndClasses/Exercise/src/Cage.java
@@ -0,0 +1,36 @@
+import animals.Animal;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Cage <T extends Animal> {
+    private List<T> animals;
+
+    public Cage(List<T> animals) {
+        this.animals = animals;
+    }
+
+    public List<T> getAnimals() {
+        return animals;
+    }
+
+    @Override
+    public String toString() {
+        return "Cage{" +
+                "animals=" + animals +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cage<?> cage = (Cage<?>) o;
+        return Objects.equals(animals, cage.animals);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(animals);
+    }
+}
