7a2a1fdef9f3b6d30741cbb524b10869bfd24a97
OzzkanBulut <ozkanb66@gmail.com>
2023-03-05 21:13:48 +0300

    giraffe

---
+++ _081_GenericAndClasses/Exercise/src/animals/Giraffe.java
@@ -0,0 +1,11 @@
+package animals;
+
+public class Giraffe extends Animal{
+
+    public Giraffe(String name, int age) {
+        super(name, age);
+        boolean isAgressive = false;
+    }
+
+
+}
