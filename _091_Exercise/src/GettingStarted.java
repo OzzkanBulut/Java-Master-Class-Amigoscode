f6d8746b69dd099df3f5ea6fee6857d3212fba84
OzzkanBulut <ozkanb66@gmail.com>
2023-03-06 20:54:14 +0300

    declarative approach

--- _091_Exercise/src/GettingStarted.java
+++ _091_Exercise/src/GettingStarted.java
@@ -2,16 +2,17 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GettingStarted {
-
+    PersonDAO personDAO = new PersonDAO();
     @Test
     public void imperativeApproach() {
+
 
         // 1. Find people aged less or equal 18
         // 2. Then change implementation to find first 10 people
 
-        PersonDAO personDAO = new PersonDAO();
         List<Person> people = personDAO.getPeople();
         List<Person> youngPeople = new ArrayList<>();
         int limit = 10;
@@ -27,7 +28,15 @@ public class GettingStarted {
             }
         }
         youngPeople.forEach(System.out::println);
+    }
 
+    @Test
+    public void declarativeApproach(){
+        List<Person> people = personDAO.getPeople();
+        List<Person> oldPeople = people.stream().filter(p -> p.getAge() >= 18).limit(10).toList();
+        List<Person> youngPeople = people.stream().filter(p -> p.getAge() <= 18).limit(10).toList();
+        youngPeople.forEach(System.out::println);
+        oldPeople.forEach(System.out::println);
 
     }
 }
