767bcda2d6634cc11d575f7971e29c7614239f05
OzzkanBulut <ozkanb66@gmail.com>
2023-03-06 23:12:18 +0300

    Java Faker Dependency

--- _092_Maven/src/main/java/org/example/Main.java
+++ _092_Maven/src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import com.github.javafaker.Faker;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello maven!");
+        Faker faker = new Faker();
+        String firstName = faker.funnyName().name();
+        String  superhero = faker.superhero().name();
+        System.out.println("The super hero is: "+superhero);
+
     }
 }
