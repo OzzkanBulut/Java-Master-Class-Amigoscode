c5693edf90dba38f50132764e262714a39e7568a
OzzkanBulut <ozkanb66@gmail.com>
2023-03-07 16:38:30 +0300

    my first test clas

---
+++ _093_TestingwithJUnit/src/test/java/MyFirstTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+public class MyFirstTest {
+
+    @Test // Needed to run the test
+    void myFirstTestMethod(){
+
+    }
+
+    @Test
+    void mySecondTestMethod(){
+
+    }
+}
