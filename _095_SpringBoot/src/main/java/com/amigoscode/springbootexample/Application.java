dd3fd1fa170723429d19ad5eae5799420e6c74fb
OzzkanBulut <ozkanb66@gmail.com>
2023-03-09 20:47:30 +0300

    first api

--- _095_SpringBoot/src/main/java/com/amigoscode/springbootexample/Application.java
+++ _095_SpringBoot/src/main/java/com/amigoscode/springbootexample/Application.java
@@ -2,12 +2,19 @@ package com.amigoscode.springbootexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController // Any method within this class that has mapping annotations, will be exposed as rest endpoints that clients can call
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
+    @GetMapping(path = "/greet")
+    public String greet(){
+        return "Hello";
+    }
 }
