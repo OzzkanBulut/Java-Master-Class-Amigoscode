ec9c3ea2c200fa298eaff826f3631dc763910481
OzzkanBulut <ozkanb66@gmail.com>
2023-09-04 10:32:44 +0300

    wrapper classes recap

--- 022- Working with Strings/src/Main.java
+++ 022- Working with Strings/src/Main.java
@@ -2,20 +2,11 @@ public class Main {
     public static void main(String[] args) {
         // Strings
         String firstName = "Özkan";
-        String lastName = " Bulut";
+        String lastName = "Bulut";
+        System.out.println(firstName.charAt(1)); // z
         System.out.println(firstName.toUpperCase());
-        System.out.println(firstName.toLowerCase());
-        System.out.println(" ".isBlank()); // true, there are spaces in the string
-        System.out.println(" ".isEmpty()); // false, there are spaces in the string
-        System.out.println(" Hello ".trim());
-        System.out.println(firstName.startsWith("Ö")); // true
-        System.out.println(firstName.substring(0,3)); // özk
-        System.out.println(firstName.replace('Ö','A')); // Replace Ö with A
-        System.out.println(firstName.charAt(2)); // k
-        System.out.println(firstName.contains("a")); // true
-
-        System.out.println(firstName.concat(lastName)); // unite the strings
-        System.out.println(firstName+ " Bulut");
+        System.out.println(firstName.indexOf("a"));//3
+        System.out.println(lastName.concat("aaa"));//Bulutaaa
 
     }
 }
