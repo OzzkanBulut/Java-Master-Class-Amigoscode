e84fdaf5fb9e3e25d27364bfe8dd5efd78fbfc4d
OzzkanBulut <67637654+OzzkanBulut@users.noreply.github.com>
2023-09-04 11:37:05 +0300

    Delete _054_StructureOfAnApplication directory

--- _054_StructureOfAnApplication/src/person/Gender.java
+++
@@ -1,6 +0,0 @@
-package person;
-
-public enum Gender {
-    MALE,
-    FEMALE
-}
--- _054_StructureOfAnApplication/src/utils/StringUtils.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-public class StringUtils {
-    public static boolean isEmpty(String s){
-        // This is pointless but mainly for example
-        return s.isEmpty();
-
-    }
-}
